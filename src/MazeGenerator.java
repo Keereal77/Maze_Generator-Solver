2eea8070a09fcdfb8868c15a338f5f4326bd1e75
Keereal77 <kirill.dobrynin78@gmail.com>
2020-11-25 18:19:53 +0200

    Skelet

---
+++ src/MazeGenerator.java
@@ -0,0 +1,12 @@
+public class MazeGenerator {
+
+    //TODO: Maze createMaze(int rowsCount, int columnCount)
+
+    //TODO: Pair<Integer, Integer> getCoordinateByVectorId(int id)
+
+    //TODO: int[][] connectVectors(int[][] mazeMatrix)
+
+    //TODO: int[][] getSpanningTree(int[][] graphMatrix)
+
+    //TODO: void fillMaze(int[][] mazeMatrix)
+}
